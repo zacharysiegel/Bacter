d87a13121649b563957dd909bab6be65d978ad83
siegelzc <siegelzc@gmail.com>
2018-12-15 01:10:12 -0600

    PRINT_DEV moved into config.json

--- src/print_dev.js
+++ src/print_dev.js
@@ -1,13 +1,14 @@
-const PRINT_DEV = false; // Developer mode for printing to the console
-
 /**
  * Same functionality as console.log() if print developer mode is enabled (PRINT_DEV === true)
  *    else do nothing
  * @param  {convertible to string} ...args Same parameter requirements as console.log()
  * @return void
  */
+
+let config = require('./config.json');
+
 let print_dev = (...args) => {
-   if (PRINT_DEV) {
+   if (config._print_dev) {
       console.log(...args);
    }
 };
