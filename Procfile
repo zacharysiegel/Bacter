fb09559df06894e1c7447c9fa864ff0cb974e361
Zachary Siegel <siegelzc@gmail.com>
2019-09-21 18:44:34 -0500

    public/bundle -> public/build

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node ./src/app.js
+web: ./start.sh --build
--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Prepares files for program execution
-#    Builds JavaScript files using Babel into a backwards-compatible bundle
+#    Builds public JavaScript files using Babel into a backwards-compatible bundle in public/build
+#    Builds server-side JavaScript files into src/build (not a bundle)
 #    Copies current configuration files into build directory
 
 echo "Copying Configurations..."
@@ -9,19 +10,22 @@ cp ./src/config.json ./src/build/config.json
 
 # If the folders containing built files do not exist, create them
 [[ ! -d ./src/build ]] && mkdir ./src/build
-[[ ! -d ./public/bundle ]] && mkdir ./public/bundle
+[[ ! -d ./public/build ]] && mkdir ./public/build
+
 # Remove the formerly built files if they exist
 [[ -e ./src/build/* ]] && rm -r ./src/build/* # Server-Side
-[[ -e ./public/bundle/* ]] && rm -r ./public/bundle/* # Client-Side
+[[ -e ./public/build/* ]] && rm -r ./public/build/* # Client-Side
 
 # Build Client-Side JavaScript into ES2015-compatible code
 echo "Building Client-Side JavaScript..."
-./node_modules/.bin/babel -o ./public/bundle/bundle.js -s true -- ./public/js ./public/lib/z.js # Package all files in /js directory into bundle.js file with source map in /bundle directory
+./node_modules/.bin/babel -o ./public/build/bundle.js -s true -- ./public/js # Package all files in /js directory into bundle.js file with source map in /bundle directory
+./node_modules/.bin/babel -d ./public/build/ -- ./public/lib/z.js
+
 # Build Server-Side JavaScript into ES2015-compatible code
 echo "Building Server-Side JavaScript..."
 ./node_modules/.bin/babel -d ./src/build/ -- ./src/*.js
 
 # Lint the bundle for errors
-# ./node_modules/.bin/eslint ./public/bundle/bundle.js
+# ./node_modules/.bin/eslint ./public/build/bundle.js
 
 exit 0
