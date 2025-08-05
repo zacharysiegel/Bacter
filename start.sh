87a78d291d50ed983598d50f6d258a760131c0aa
Zachary Siegel <siegelzc@gmail.com>
2025-08-05 13:22:01 -0500

    remove authbind

--- start.sh
+++ start.sh
@@ -4,10 +4,6 @@
 # @option --help                     Prints help message and exits with code 0
 # @option -b / --build               Runs ./build.sh before starting the server
 
-function command_exists {
-    type -P "$1" 1> /dev/null 2> /dev/null;
-}
-
 # Check for options/arguments
 for ((i=1; i<=$#; i++)); do
     if [[ ${!i} = "--help" ]]; then
@@ -37,26 +33,17 @@ if [[ $build = "true" ]]; then
     echo
 fi
 
-# authbind (I believe) creates a file named 'localhost:80' which blocks express from listening on port 80
-test -e localhost:80 && rm localhost:80
-
 if grep -- project_state ./src/config/config.json | grep -q -- production # If the "project_state" property in ./src/config.json is set to "production"
 then
     echo "Project state is set to production"
     echo "JavaScript is ES2015-compatible"
     echo
 
-    if command_exists authbind # authbind may not be present on the Heroku machine
-    then
-        authbind node ./src/build/app.js # Production
-    else # try without authbind
-        echo "[WARN] :: The 'authbind' program is not present"
-        node ./src/build/app.js
-    fi
+    node ./src/build/app.js # Production
 else
     echo "Project state is set to development"
     echo
-    authbind node ./src/app.js # Development (ES2015-Compliant code is not needed in development)
+    node ./src/app.js # Development (ES2015-Compliant code is not needed in development)
 fi
 
 exit 0
